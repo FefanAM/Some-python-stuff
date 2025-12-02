955b914c3cfcdbc6e2799aaf6f4e3270cd9d705f
FefanAM <czmikton2020@gmail.com>
2025-12-02 11:36:20 +0100

    idk bro

---
+++ Diagramy/linearni-rovnice.py
@@ -0,0 +1,10 @@
+def solve_linear(a, b):
+    a = str(a)
+    b = str(b)
+    if a == 0 and b == 0:
+        return "Rovnice ma nekonecne reseni"
+    if a == 0 and b != 0:
+        return "Rovnice nema reseni"
+    return -b / a
+
+print(solve_linear(input('Zadej koeficient a:'), input('Zadej koeficient b:')))
