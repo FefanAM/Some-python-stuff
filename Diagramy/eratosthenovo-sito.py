a02534de2e954d9c62aeacaf7d20bcaf8a549f4b
FefanAM <czmikton2020@gmail.com>
2026-01-06 20:16:53 +0100

    optimize eratosthenovo-sito.py

--- Diagramy/eratosthenovo-sito.py
+++ Diagramy/eratosthenovo-sito.py
@@ -1,41 +1,38 @@
-def eratosthenes_sieve(n):
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
+import time
 
-    p = 2
+
+def eratosthenes_sieve(n):
+    is_prime = [True] * (n // 2 + 1)
+    is_prime[0] = False
+
+    p = 3
     while p * p <= n:
-        if p == 2:
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-            p += 1
-            continue
         if is_prime[p]:
-            for i in range(p * p, n + 1, p * 2):
+            for i in range(p * p, n // 2 + 1, p):
                 is_prime[i] = False
         p += 1
 
-    primes = []
-    for p in range(n + 1):
+    primes = [2]
+    for p in range(len(is_prime)):
         if is_prime[p]:
-            primes.append(p)
+            primes.append(p * 2 + 1)
     return primes
 
 
 def sito(n):
-    number_row = [*range(2, n + 1)]
+    number_row = [*range(1, n + 1, 2)]
 
     for num in range(2, int(n ** 0.5) + 1):
         if num not in number_row:
             continue
-        if num == 2:
-            for i in range(num * num, n + 1, num):
-                if i in number_row:
-                    number_row.remove(i)
         else:
-             for i in range(num * num, n + 1, num * 2):
+            for i in range(num * num, n + 1, num * 2):
                 if i in number_row:
                     number_row.remove(i)
 
     return number_row
 
-print(eratosthenes_sieve(300000000))
+
+is_time = time.time()
+eratosthenes_sieve(1000000000)
+print(f'Process took {int(time.time() - is_time)} seconds.')
