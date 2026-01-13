3f9ce2e0bcfbea6cbe23262eec900ebad0ff4bbd
FefanAM <czmikton2020@gmail.com>
2026-01-13 11:08:32 +0100

    better fibonacci sequence

--- Diagramy/fibonacci.py
+++ Diagramy/fibonacci.py
@@ -1,22 +1,28 @@
 import time
+from textwrap import shorten
 
 
-def fib(limit, get_time=False, only_last=False):
+def fib(limit, get_time=False, shorten_number=False):
     i = 1
-    nums = [1, 2]
     a, b = 1, 2
-    if limit == 1:
+    if limit == 1 or limit == 0:
         return 1
     seconds = time.time()
     while i < limit - 1:
         a, b = b, a + b
-        nums.append(b)
         i += 1
-    if only_last:
-        nums = nums[len(nums) - 1]
+    if shorten_number and len(str(b)) > 6:
+        b = f'{int(str(b)[:2])} × 10^{len(str(b)) - 2}'
     if get_time:
-        return nums, time.time() - seconds
-    return nums
+        return b, time.time() - seconds
+    return b
 
 
-print(fib(1000, only_last=True))
+def fib_recursion(n):
+    if n == 0 or n == 1:
+        return 1
+    return fib_recursion(n - 1) + fib_recursion(n - 2)
+
+
+print(fib(10000, get_time=True, shorten_number=True))
+print(fib_recursion(10))
