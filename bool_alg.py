d0ccc5bba37c54b9de0dfe556eb6f96a4708b579
FefanAM <czmikton2020@gmail.com>
2026-02-02 08:35:17 +0100

    comment every single thing

--- bool_alg.py
+++ bool_alg.py
@@ -8,35 +8,37 @@ TODO:
 
 
 def solve(func: str, n: int):
+    # combine everything to return whole table with answer for given function (func)
     tab = table(n)
     ans = []
     temp_list = []
     is_negated = False
+    # make the string with function uniform for next operations
     mult = func.replace(' ', '')
     mult = mult.replace('*', '')
     mult = mult.upper()
     mult = mult.replace('_', '!')
     mult = mult.replace('NOT', '!')
-    mult = mult.split('+')
+    mult = mult.split('+')  # split into multiplications
     for m in mult:
-        temp_list = []
+        temp_list = []  # clear the temporary list used for multiplication
         for var in m:
-            if var == '!':
+            if var == '!':  # set next variable to be negated
                 is_negated = True
                 continue
-            if is_negated:
+            if is_negated:  # negate variable
                 temp_list.append(negation(get_truth_list(var, tab)))
                 is_negated = False
-            else:
+            else:  # append normal variable
                 temp_list.append(get_truth_list(var, tab))
-        m = m.replace('!', '')
-        if len(m) < 2:
+        m = m.replace('!', '')  # get rid of negation
+        if len(m) < 2:  # if length less than two (no multiplication needed), return single variable
             ans.append(temp_list[0])
             continue
-        ans.append(bool_mult(temp_list, tab))
-    if len(mult) < 2:
+        ans.append(bool_mult(temp_list, tab))  # multiply the values and append to answer list
+    if len(mult) < 2:  # if length less than two (no addition needed), return single variable
         return print_table(tab, ans[0])
-    return print_table(tab, bool_add(ans, tab))
+    return print_table(tab, bool_add(ans, tab))  # add contents of answer list
     
         
 
@@ -57,6 +59,8 @@ def table(n: int) -> list:
 
 
 def bool_add(variables: list, b_table: list) -> list:
+    # use 'or' operator to add two lists
+    # works same as multiplication
     answer = []
     if len(variables) == 2:
         for a, b in zip(variables[0], variables[1]):
@@ -77,32 +81,35 @@ def bool_add(variables: list, b_table: list) -> list:
 
 
 def bool_mult(variables: list, b_table: list) -> list:
+    # use 'and' operator to multiply two lists
     answer = []
-    if len(variables) == 2:
+    if len(variables) == 2:  # if there are two variables, multiply them
         for a, b in zip(variables[0], variables[1]):
-            if bool(int(a)) and bool(int(b)):
+            if bool(int(a)) and bool(int(b)):  # give result of multiplication
                 answer.append(1)
             else:
                 answer.append(0)
         return answer
-    for a, b in zip(variables[0], variables[1]):
+    for a, b in zip(variables[0], variables[1]):  # if there are more than two variables, multiply first two and use recursion to multiply result with next item until solved
             if bool(int(a)) and bool(int(b)):
                 answer.append(1)
             else:
                 answer.append(0)
+    # remove first two (already multiplied) variables
     variables.pop(0)
     variables.pop(1)
-    variables.insert(0, answer)
+    variables.insert(0, answer)  # add result to index 0 of variables
     return bool_mult(variables, b_table)
 
 
 def get_truth_list(n: str, reference: list) -> list:
+    # returns list of values for given variable based on table
     answer = []
-    c = int(letters.index(n) / 2 + 1)
+    c = int(letters.index(n) / 2 + 1)  # position number of variable 
     for x in reference:
         i = 0
         for y in x:
-            if i == c - 1:
+            if i == c - 1:  # only append for right column
                 answer.append(y)
                 i += 1
             else:
@@ -114,9 +121,10 @@ def get_truth_list(n: str, reference: list) -> list:
 def print_table(a: list, y: list = []) -> str:
     # takes the inputted table list and unpacks it into a more readable form
     if not y:
-        y = ['a'] * len(a)
+        y = [' '] * len(a)  # if function not present, add spaces in its place
     result = f'{letters[:len(a[0]) * 2 - 1]} | Y\n{"-" * (len(a[0]) * 2 + 3)}'
     for row, i in zip(a, y):
+        # loop through both the table and answer
         row.append('|')
         row.append(str(i))
         result = f'{result}\n{" ".join(row)}'  # removes the square brackets and starts a new line after every row
@@ -124,9 +132,10 @@ def print_table(a: list, y: list = []) -> str:
 
 
 def negation(l: list) -> list:
+    # takes a list of values and returns opposite values
     answer = []
     for item in l:
-        answer. append(int(not bool(int(item))))
+        answer. append(int(not bool(int(item))))  # convert item of list to bool, change it to opposite and convert back to number
     return answer
 
 
