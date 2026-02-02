4f4aa9bb75590ec6e1972775bd60be4476c79c26
FefanAM <czmikton2020@gmail.com>
2026-02-02 20:18:17 +0100

    fix multiplication and addition not working for more than two variables; add exceptions for invalid characters; solve no longer needs n (number of variables), which is obtained from provided function

--- bool_alg.py
+++ bool_alg.py
@@ -1,27 +1,36 @@
 letters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 
-""" 
-TODO:
-    implement solving for final operation = multiplication
-    brackets
-"""
 
-
-def solve(func: str = '', n: int = 2):
+def solve(func: str = ''):
     # combine everything to return whole table with answer for given function (func)
-    tab = table(n)
-    ans = []
-    temp_list = []
-    is_negated = False
-    if func == '':  # if no function given, return table for n variables
-        return print_table(a=tab)
+
     # make the string with function uniform for next operations
     mult = func.replace(' ', '')
     mult = mult.replace('*', '')
     mult = mult.upper()
     mult = mult.replace('_', '!')
     mult = mult.replace('NOT', '!')
+
+    # get number of unique variables in function and set it as n
+    n_vars = mult.replace('+', '')
+    n_vars = n_vars.replace('!', '')
+    n = len(set(n_vars))
+
+    tab = table(n)
+    ans = []
+    is_negated = False
+    if func.isdigit():  # if no function given, return table for n variables
+        return print_table(a=table(int(func)))
+
+    for var in n_vars:
+        try:
+            if letters.index(var) / 2 + 1 > n:
+                return 'Please enter variables in alphabetic order beginning with "A".'
+        except ValueError:
+            return 'Invalid character.'
+
     mult = mult.split('+')  # split into multiplications
+
     for m in mult:
         temp_list = []  # clear the temporary list used for multiplication
         for var in m:
@@ -41,8 +50,7 @@ def solve(func: str = '', n: int = 2):
     if len(mult) < 2:  # if length less than two (no addition needed), return single variable
         return print_table(tab, ans[0])
     return print_table(tab, bool_add(ans, tab))  # add contents of answer list
-    
-        
+
 
 def table(n: int) -> list:
     # returns a list of rows based on the number of variables (n)
@@ -72,12 +80,12 @@ def bool_add(variables: list, b_table: list) -> list:
                 answer.append(0)
         return answer
     for a, b in zip(variables[0], variables[1]):
-            if bool(int(a)) or bool(int(b)):
-                answer.append(1)
-            else:
-                answer.append(0)
+        if bool(int(a)) or bool(int(b)):
+            answer.append(1)
+        else:
+            answer.append(0)
     variables.pop(0)
-    variables.pop(1)
+    variables.pop(0)
     variables.insert(0, answer)
     return bool_add(variables, b_table)
 
@@ -93,13 +101,13 @@ def bool_mult(variables: list, b_table: list) -> list:
                 answer.append(0)
         return answer
     for a, b in zip(variables[0], variables[1]):  # if there are more than two variables, multiply first two and use recursion to multiply result with next item until solved
-            if bool(int(a)) and bool(int(b)):
-                answer.append(1)
-            else:
-                answer.append(0)
+        if bool(int(a)) and bool(int(b)):
+            answer.append(1)
+        else:
+            answer.append(0)
     # remove first two (already multiplied) variables
     variables.pop(0)
-    variables.pop(1)
+    variables.pop(0)
     variables.insert(0, answer)  # add result to index 0 of variables
     return bool_mult(variables, b_table)
 
@@ -107,22 +115,23 @@ def bool_mult(variables: list, b_table: list) -> list:
 def get_truth_list(n: str, reference: list) -> list:
     # returns list of values for given variable based on table
     answer = []
-    c = int(letters.index(n) / 2 + 1)  # position number of variable 
+    c = int(letters.index(n) / 2 + 1)  # position number of variable
     for x in reference:
         i = 0
         for y in x:
             if i == c - 1:  # only append for right column
                 answer.append(y)
                 i += 1
+                break
             else:
                 i += 1
                 continue
     return answer
 
 
-def print_table(a: list, y: list = []) -> str:
+def print_table(a: list, y=None) -> str:
     # takes the inputted table list and unpacks it into a more readable form
-    if not y:
+    if y is None:
         y = [' '] * len(a)  # if function not present, add spaces in its place
     result = f'{letters[:len(a[0]) * 2 - 1]} | Y\n{"-" * (len(a[0]) * 2 + 3)}'
     for row, i in zip(a, y):
@@ -133,12 +142,13 @@ def print_table(a: list, y: list = []) -> str:
     return result
 
 
-def negation(l: list) -> list:
+def negation(li: list) -> list:
     # takes a list of values and returns opposite values
     answer = []
-    for item in l:
-        answer. append(int(not bool(int(item))))  # convert item of list to bool, change it to opposite and convert back to number
+    for item in li:
+        answer.append(int(not bool(int(item))))  # convert item of list to bool, change it to opposite and convert back to number
     return answer
 
 
-print(solve(n=2))
+while True:
+    print(solve(input('Enter function: Y = ')))
