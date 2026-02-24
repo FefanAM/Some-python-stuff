40f06454ee3da9c5290bf1a4bb908d458f90b7e4
FefanAM <czmikton2020@gmail.com>
2026-02-24 20:23:47 +0100

    breakout now has most core functions

--- Pygame/breakout.py
+++ Pygame/breakout.py
@@ -1,7 +1,5 @@
 import math
-
 import pygame
-import random
 
 # pygame setup
 pygame.init()
@@ -10,17 +8,19 @@ height = 720
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
-all_sprites_list = pygame.sprite.Group()
+block_list = pygame.sprite.Group()
+balls = pygame.sprite.Group()
 dt = 0
 
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, n, m):
+    def __init__(self, n, m, health):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
-        block_width = 100
-        block_height = 30
+        self.width = 100
+        self.height = 30
+        self.hp = health
         colors = ["red", "orange", "yellow", "green", "blue", "indigo"]
         if m >= len(colors):
             c = m - len(colors)
@@ -28,18 +28,32 @@ class Block(pygame.sprite.Sprite):
             c = m
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([block_width, block_height])
+        self.image = pygame.Surface([self.width, self.height])
         self.image.fill(colors[c])
 
         # Fetch the rectangle object that has the dimensions of the image
         # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.image.get_rect()
-        self.rect.x = 10 + n * (block_width + 10)
-        self.rect.y = 10 + m * (block_height + 5)
+        self.rect.x = 10 + n * (self.width + 10)
+        self.rect.y = 10 + m * (self.height + 5)
+
+    def damage(self):
+        self.hp -= 1
+        if self.hp <= 0:
+            self.kill()
+            blocks.remove(self)
+
+    @property
+    def x(self):
+        return self.rect.x
+
+    @property
+    def y(self):
+        return self.rect.y
 
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, size, speed):
+    def __init__(self, size, speed, angle):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
@@ -47,7 +61,9 @@ class Ball(pygame.sprite.Sprite):
         # This could also be an image loaded from the disk.
 
         self.size = size
-        self.angle = 45
+        self.angle = angle
+        self.bx = self.by = 1
+        self.padding = 5
         self.image = pygame.Surface([self.size, self.size], pygame.SRCALPHA)
         pygame.draw.circle(self.image, 'white', (self.size // 2, self.size // 2), self.size // 2)
         self.image = self.image.convert_alpha()
@@ -60,8 +76,28 @@ class Ball(pygame.sprite.Sprite):
         self.rect.y = height // 2 + 200
 
     def move(self):
-        self.x += math.cos(self.angle) * self.speed * dt
-        self.y -= math.sin(self.angle) * self.speed * dt
+        self.x += math.cos(self.angle) * self.speed * dt * self.bx
+        self.y -= math.sin(self.angle) * self.speed * dt * self.by
+
+    def collision(self, col):
+        if self.x == 0 or self.x == width - self.size:
+            self.bx *= -1
+        if self.y == 0:
+            self.by *= -1
+
+        collisions = self.rect.collideobjectsall(col)
+        for c in collisions:
+            if isinstance(c, Player):
+                self.by *= -1
+                self.y = c.y - self.size
+                return
+            if c.x + c.width > self.x + self.padding > c.x or c.x + c.width > self.x + self.size - self.padding > c.x:
+                self.by *= -1
+            elif c.y + c.height > self.y + self.padding > c.y or c.y + c.height > self.y + self.size - self.padding > c.y:
+                self.bx *= -1
+            if isinstance(c, Block):
+                c.damage()
+            return
 
     @property
     def x(self):
@@ -96,8 +132,9 @@ class Player(pygame.sprite.Sprite):
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
 
-        self.width = 100
-        self.image = pygame.Surface([self.width, 30])
+        self.width = 200
+        self.height = 10
+        self.image = pygame.Surface([self.width, self.height])
         self.image.fill('white')
         self.speed = 1000
 
@@ -105,7 +142,7 @@ class Player(pygame.sprite.Sprite):
         # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.image.get_rect()
         self.rect.x = width // 2 - self.width // 2
-        self.rect.y = height - 50
+        self.rect.y = height - 100
 
     def left(self):
         self.x -= self.speed * dt
@@ -121,16 +158,21 @@ class Player(pygame.sprite.Sprite):
     def x(self, value):
         self.rect.x = min(max(0, value), width - self.width)
 
+    @property
+    def y(self):
+        return self.rect.y
 
+
+blocks = []
 for y in range(int((height / 2) / 30)):
     for x in range(int(width / 100)):
-        all_sprites_list.add((Block(x, y)))
+        blocks.append((Block(x, y, 1)))
 
 player = Player()
-all_sprites_list.add(player)
+blocks.append(player)
+block_list.add(blocks)
 
-ball = Ball(30, 1000)
-all_sprites_list.add(ball)
+balls.add(Ball(30, 600, 45))
 
 while running:
     # poll for events
@@ -150,12 +192,15 @@ while running:
         ball.phi -= 10 * dt
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
-    ball.move()
-    all_sprites_list.draw(screen)
+    for ball in balls:
+        ball.move()
+        ball.collision(blocks)
+    block_list.draw(screen)
+    balls.draw(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    dt = clock.tick(60) / 1000  # limits FPS to 60
+    dt = clock.tick(400) / 1000
 
 pygame.quit()
