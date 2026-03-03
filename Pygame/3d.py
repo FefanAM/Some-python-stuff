aed2ee5cebea5da3819247e492fa458d10e4637e
FefanAM <czmikton2020@gmail.com>
2026-03-03 11:32:49 +0100

    solve rotation for single points, connecting said points does not work

--- Pygame/3d.py
+++ Pygame/3d.py
@@ -1,25 +1,104 @@
+import math
+
 import pygame
 import numpy as np
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+width = 1280
+height = 720
+alpha = beta = gam = 0
+dt = 0
+
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
 project = np.array([[1, 0, 0], [0, 1, 0]])
-point1 = np.array([[50], [50], [0]])
+
+points = [
+    np.array([[50], [50], [50]]),
+    np.array([[50], [-50], [50]]),
+    np.array([[-50], [50], [50]]),
+    np.array([[-50], [-50], [50]]),
+    np.array([[50], [50], [-50]]),
+    np.array([[50], [-50], [-50]]),
+    np.array([[-50], [50], [-50]]),
+    np.array([[-50], [-50], [-50]])
+]
+
+
+def connect_dots(dots):
+    for dot in dots:
+        for d in dots:
+            if dot.ravel()[0] == d.ravel()[0] and dot.ravel()[1] == d.ravel()[1] or dot.ravel()[0] == d.ravel()[0] and dot.ravel()[2] == d.ravel()[2] or dot.ravel()[2] == d.ravel()[2] and dot.ravel()[1] == d.ravel()[1]:
+                pygame.draw.line(screen, 'white', to_2d(dot), to_2d(d))
+
+
+def c(coordinates):
+    return [coordinates[0] + width // 2, height // 2 - coordinates[1]]
+
+
+def rotate_x(point, angle):
+    rotation = np.array(
+        [[1, 0, 0],
+         [0, math.cos(angle), - math.sin(angle)],
+         [0, math.sin(angle), math.cos(angle)]]
+    )
+    return np.matmul(rotation, point)
+
+
+def rotate_y(point, angle):
+    rotation = np.array(
+        [[math.cos(angle), 0, math.sin(angle)],
+         [0, 1, 0],
+         [- math.sin(angle), 0, math.cos(angle)]]
+    )
+    return np.matmul(rotation, point)
+
+
+def rotate_z(point, angle):
+    rotation = np.array(
+        [[math.cos(angle), - math.sin(angle), 0],
+         [math.sin(angle), math.cos(angle), 0],
+         [ 0, 0, 1]]
+    )
+    return np.matmul(rotation, point)
+
+
+def rotate_somehow(point, angle):
+    rotation = np.array(
+        [[math.cos(angle), - math.sin(angle), 0],
+         [math.sin(angle), 1, math.cos(angle)],
+         [ 0, 0, 1]]
+    )
+    return np.matmul(rotation, point)
+
+
+
+def to_2d(point):
+    return c(np.matmul(project, point).ravel())
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("purple")
+    screen.fill("black")
 
-    pygame.draw.circle(screen, 'white', np.matmul(project, point1).ravel(), 20)
+    points_lines = []
+
+    for p in points:
+        p = rotate_x(rotate_y(rotate_z(p, gam), beta), alpha)
+        points_lines.append(p)
+        pygame.draw.circle(screen, 'white', to_2d(p), 20)
+
+    connect_dots(points_lines)
 
     pygame.display.flip()
-
+    alpha += 3 * dt
+    beta += 2 * dt
+    gam += 1 * dt
     dt = clock.tick(60) / 1000
 
 pygame.quit()
